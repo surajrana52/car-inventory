f7546b2f4342bffa30e7256058f8f064aa7e03a0
Suraj <suraj@minkspay.com>
2018-12-09 11:42:31 +0530

    Created header & footer files along with index.php

---
+++ views/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mini Car Inventory</title>
+
+    <link rel="stylesheet" href="../assests/css/bootstrap.min.css">
+
+</head>
+<body>
+
+
+<script src="../assests/js/jquery-3.3.1.min.js"></script>
+<script src="../assests/js/bootstrap.min.js"></script>
+
+<script>
+    $(document).ready(function (e) {
+        console.log("hello");
+    });
+</script>
+
+</body>
+</html>
