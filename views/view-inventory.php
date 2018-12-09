83b04ef00cfb4d6cb359bc50c2cefa965582b0c5
Suraj <suraj@minkspay.com>
2018-12-09 13:25:14 +0530

    created view-inventory page

---
+++ views/view-inventory.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Inventory</title>
+
+    <link rel="stylesheet" href="../assests/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assests/css/custom.css">
+
+</head>
+<body>
+
+<?php
+include('layouts/header.php');
+?>
+
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
+
+
+<script src="../assests/js/jquery-3.3.1.min.js"></script>
+<script src="../assests/js/bootstrap.min.js"></script>
+
+<script>
+    $(document).ready(function (e) {
+        console.log("hello");
+    });
+</script>
+
+</body>
+</html>
